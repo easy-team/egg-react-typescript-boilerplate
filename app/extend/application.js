510e09c95b858b1d0d51358e8a6b220d09bfd5df
sky <hubcarl@126.com>
2023-04-01 09:58:04 +0800

    chore: changelog

--- app/extend/application.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const factory_1 = require("../lib/db/factory");
-const DBSymbol = Symbol('Application#db');
-exports.default = {
-    get db() {
-        return factory_1.default();
-    }
-};

d8af688129984f5aab585e6847fee827ff1db007
sky <hubcarl@126.com>
2024-09-08 21:26:12 +0800

    chore: remove js temp file

--- app/extend/application.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const factory_1 = require("../lib/db/factory");
-const DBSymbol = Symbol('Application#db');
-exports.default = {
-    get db() {
-        return (0, factory_1.default)();
-    }
-};
